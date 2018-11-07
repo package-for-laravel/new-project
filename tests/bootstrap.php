b520b3fe74cdc9da7212d0ab0dae7f0d90333f63
Aaron Saray <me@aaronsaray.com>
2018-11-07 15:14:37 -0600

    added global mockery helpers

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The bootstrap file for PHP Unit
+ */
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Get the helpers for mockery
+ */
+Mockery::globalHelpers();

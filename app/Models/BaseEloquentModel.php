e9853406b66d46d9faf909873a36a8d6502c00e5
Aaron Saray <me@aaronsaray.com>
2018-11-14 13:22:21 -0600

    adding my base eloquent model

---
+++ app/Models/BaseEloquentModel.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * My base eloquent model
+ */
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BaseEloquentModel
+ * @package App\Models
+ */
+abstract class BaseEloquentModel extends Model
+{
+    /**
+     * Get the table name of this model
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return with(new static())->getTable();
+    }
+}

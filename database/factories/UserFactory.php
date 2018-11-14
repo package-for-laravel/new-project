8323f16730d2a53d39bf0dd409e993ba6a223e88
Aaron Saray <me@aaronsaray.com>
2018-11-14 13:49:03 -0600

    added factory hint

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 declare(strict_types=1);
 
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use Faker\Generator as Faker;
 |
 */
 
+/** @var Factory $factory */
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,

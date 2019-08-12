1a3300fced485c32a88b8a165bd0c23c6bccc406
Aaron Saray <me@aaronsaray.com>
2019-08-12 10:35:35 -0500

    added some web route item notes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
+declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// use Route::view so it can be cached
+Route::view('', 'welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// non-changing data?
+// Route::middleware('cache.headers:public;max_age=2628000')
